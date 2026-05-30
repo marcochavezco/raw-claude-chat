9bcbbc1f521d3481246d7d60b855eef81b0d5c60
marcochavezco <machavezco@gmail.com>
2026-05-29 18:56:55 -0600

    chat loop

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,29 @@
-async function chat(message: string) {
-  const URL = `https://api.anthropic.com/v1/messages`;
+import { createInterface } from 'node:readline';
+import { stdin } from 'node:process';
+
+const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY;
+
+type Message = {
+  role: 'user' | 'assistant';
+  content: string;
+};
+
+async function chat(messages: Message[]) {
+  const URL = 'https://api.anthropic.com/v1/messages';
 
   try {
     const res = await fetch(URL, {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
-        'x-api-key': `${process.env.ANTHROPIC_API_KEY}`,
+        'x-api-key': ANTHROPIC_API_KEY as string,
         'anthropic-version': '2023-06-01',
       },
       body: JSON.stringify({
         model: 'claude-sonnet-4-5',
         max_tokens: 1024,
-        messages: [
-          {
-            role: 'user',
-            content: 'Hello Claude',
-          },
-        ],
+        system: 'Respond every message in Spanish.',
+        messages,
       }),
     });
 
@@ -29,13 +35,45 @@ async function chat(message: string) {
 
     return data;
   } catch (error: any) {
-    return console.error(error.message);
+    console.error(error.message);
+    return null;
   }
 }
 
-async function main() {
-  const response = await chat('test');
-  console.log(response.content[0].text);
+const history: Message[] = [];
+
+async function conversation(message: Message) {
+  history.push(message);
+  try {
+    const response = await chat(history);
+    const assistantMessage = {
+      role: 'assistant' as const,
+      content: response.content[0].text,
+    };
+    history.push(assistantMessage);
+    return assistantMessage;
+  } catch (error: any) {
+    console.log(error.message);
+  }
 }
 
-main();
+async function startLoop() {
+  const rl = createInterface({
+    input: stdin,
+  });
+
+  rl.question('You: ', async (answer: string) => {
+    if (answer.toLowerCase() === 'exit') {
+      console.log('Exiting...');
+      rl.close();
+      return;
+    }
+
+    const response = await conversation({ role: 'user', content: answer });
+    console.log('Assintant: ', response?.content);
+
+    await startLoop();
+  });
+}
+
+startLoop();
